dab681d2c4be0d5d4ca33b207bda712e8ba6680a
kanghyoseong <heydotory@gmail.com>
2023-03-13 21:35:05 +0900

    ImageSwitcher 프로젝트 생성
    버튼 레이아웃 구성 완료

---
+++ 05_ImageSwitcher/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/imageswitcher/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.tukorea.www.smgp2018184001.imageswitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        setContentView(R.layout.activity_main);
+    }
+}

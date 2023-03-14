5784a6f1d6857bceb43e753c2719f32aa78e2d04
kanghyoseong <heydotory@gmail.com>
2023-03-14 14:16:13 +0900

    버튼 누르면 로그 생성

--- 05_ImageSwitcher/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/imageswitcher/MainActivity.java
+++ 05_ImageSwitcher/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/imageswitcher/MainActivity.java
@@ -4,13 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        Log.d(TAG, "Prev Pressed");
+    }
+
+    public void onBtnNext(View view) {
+        Log.d(TAG, "Next Pressed");
     }
 }

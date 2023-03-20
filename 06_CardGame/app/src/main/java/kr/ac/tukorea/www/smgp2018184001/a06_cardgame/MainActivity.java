6bde5dbbcff89c6678f4dba79fb51e7b09eec6bd
kanghyoseong <heydotory@gmail.com>
2023-03-20 17:05:08 +0900

    Flip 횟수 출력, Restart 버튼 추가, 경고창 띄우기

--- 06_CardGame/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a06_cardgame/MainActivity.java
+++ 06_CardGame/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a06_cardgame/MainActivity.java
@@ -1,11 +1,14 @@
 package kr.ac.tukorea.www.smgp2018184001.a06_cardgame;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
@@ -42,12 +45,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreTextView = findViewById((R.id.scoreTextView));
         for(int i = 0; i < BUTTON_IDS.length; i++) {
             ImageButton btn = findViewById(BUTTON_IDS[i]);
             btn.setTag(resIds[i]);
         }
     }
     private ImageButton previousImgButton; // 마지막으로 눌렀던 버튼
+    private TextView scoreTextView;
+    private int flips = 0;
     public void onBtnCard(View view) {
         Log.d(TAG, "Card ID = " + view.getId());
         int cardIndex = getIndexWithId(view.getId());
@@ -73,6 +79,31 @@ public class MainActivity extends AppCompatActivity {
                 previousImgButton.setImageResource(R.mipmap.card_blue_back);
             }
         }
+        setFlips(flips+1);
         previousImgButton = btn;
+    }
+
+    private void setFlips(int flips){
+        this.flips = flips;
+        scoreTextView.setText("Flips: " + flips);
+    }
+
+    public void onBtnRestart(View view) {
+        askRetry();
+    }
+    private void askRetry(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Restart");
+        builder.setMessage("Do you really want to restart the game?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Log.d(TAG, "Restart Here");
+            }
+        });
+        builder.setNegativeButton("No", null);
+
+        AlertDialog dlg = builder.create();
+        dlg.show();
     }
 }

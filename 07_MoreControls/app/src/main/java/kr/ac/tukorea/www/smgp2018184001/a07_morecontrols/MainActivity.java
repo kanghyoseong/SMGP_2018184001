1ea2fb9deedf7a0041d09c88792c4664867c2bb8
kanghyoseong <heydotory@gmail.com>
2023-03-21 14:53:42 +0900

    More Controls 프로젝트 생성

---
+++ 07_MoreControls/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a07_morecontrols/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.www.smgp2018184001.a07_morecontrols;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

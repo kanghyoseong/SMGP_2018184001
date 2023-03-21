93e5dee7b20115d81075a45128f02a4a4dad1ada
kanghyoseong <heydotory@gmail.com>
2023-03-21 13:55:40 +0900

    재시작 기능 추가, 같은 카드 선택시 Toast 보여주기

--- 06_CardGame/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a06_cardgame/MainActivity.java
+++ 06_CardGame/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a06_cardgame/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
@@ -49,15 +50,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scoreTextView = findViewById((R.id.scoreTextView));
+        startGame();
+    }
+
+    private void startGame(){
+        setFlips(0);
         for (int i = 0; i < BUTTON_IDS.length; i++) {
             ImageButton btn = findViewById(BUTTON_IDS[i]);
             btn.setTag(resIds[i]);
+            btn.setVisibility(View.VISIBLE);
+            btn.setImageResource(R.mipmap.card_blue_back);
         }
+        openCardCount = BUTTON_IDS.length;
     }
 
     private ImageButton previousImgButton; // 마지막으로 눌렀던 버튼
     private TextView scoreTextView;
     private int flips = 0;
+    private int openCardCount = 0;
 
     public void onBtnCard(View view) {
         Log.d(TAG, "Card ID = " + view.getId());
@@ -66,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
 
         ImageButton btn = (ImageButton) view;
         if (btn == previousImgButton) {
+            //같은 카드가 눌리면 Toast를 보여준다.
+            Toast.makeText(this, "Same Card", Toast.LENGTH_SHORT).show();
             return;
         }
         int resId = (Integer) btn.getTag();
@@ -78,6 +90,10 @@ public class MainActivity extends AppCompatActivity {
                 btn.setVisibility(View.INVISIBLE);
                 previousImgButton.setVisibility((View.INVISIBLE));
                 previousImgButton = null;
+                openCardCount -= 2;
+                if (openCardCount <= 0) {
+                    askRetry();
+                }
                 return;
             } else {
                 previousImgButton.setImageResource(R.mipmap.card_blue_back);
@@ -104,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         Log.d(TAG, "Restart Here");
+                        startGame();
                     }
                 })
                 .setNegativeButton("No", null)

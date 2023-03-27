b8eb0008e7be5b5e2487460979987f3245a45e3a
kanghyoseong <heydotory@gmail.com>
2023-03-27 21:06:16 +0900

    화면 중앙에 공 그리기

--- 07_MoreControls/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a07_morecontrols/BallView.java
+++ 07_MoreControls/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a07_morecontrols/BallView.java
@@ -1,10 +1,14 @@
 package kr.ac.tukorea.www.smgp2018184001.a07_morecontrols;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -15,6 +19,8 @@ import android.view.View;
  */
 public class BallView extends View {
     private Paint paint = new Paint();
+    private Bitmap ballBitmap;
+    private RectF ballRect = new RectF();
 
     public BallView(Context context) {
         super(context);
@@ -36,6 +42,9 @@ public class BallView extends View {
         paint.setStrokeWidth(5);
         paint.setTextSize(100);
         paint.setColor(Color.BLUE);
+
+        Resources res = getResources();
+        ballBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
     }
 
     @Override
@@ -53,7 +62,15 @@ public class BallView extends View {
         int contentHeight = getHeight() - paddingTop - paddingBottom;
 
         canvas.drawRect(paddingLeft, paddingTop,
-                paddingLeft+contentWidth, paddingTop+contentHeight, paint);
-        canvas.drawText("Hello, Custom View", paddingLeft+100, paddingTop+100, paint);
+                paddingLeft + contentWidth, paddingTop + contentHeight, paint);
+        canvas.drawText("Hello, Custom View", paddingLeft + 100, paddingTop + 100, paint);
+
+        //cx, cy: center x, y
+        int cx = paddingLeft + contentWidth / 2;
+        int cy = paddingTop + contentHeight / 2;
+        //공 반지름 = 화면 폭 / 8
+        int radius = contentWidth / 8;
+        ballRect.set(cx - radius, cy - radius, cx + radius, cy + radius);
+        canvas.drawBitmap(ballBitmap, null, ballRect, null);
     }
 }

d685f5f47a1e4ffee3086f84ba4b0bde6da73ee8
kanghyoseong <heydotory@gmail.com>
2023-03-27 16:41:59 +0900

    새 Custom View에서 사각형 그리기

---
+++ 07_MoreControls/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a07_morecontrols/GameView.java
@@ -0,0 +1,59 @@
+package kr.ac.tukorea.www.smgp2018184001.a07_morecontrols;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class GameView extends View {
+
+    public GameView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+
+        Rect rect = new Rect(paddingLeft, paddingTop, getWidth()-paddingRight, getHeight()-paddingBottom);
+        canvas.drawRect(rect, paint);
+    }
+}

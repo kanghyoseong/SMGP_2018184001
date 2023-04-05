4eb2aa7c15bdb73cf0019b3e7f0aa01d5214759f
kanghyoseong <heydotory@gmail.com>
2023-04-05 13:27:56 +0900

    기존의 따라하기 프로젝트 삭제

--- 08_sampleGame/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/a08_samplegame/IGameObject.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.tukorea.www.smgp2018184001.a08_samplegame;
-
-import android.graphics.Canvas;
-
-public interface IGameObject {
-    public void update();
-    public void draw(Canvas canvas);
-}

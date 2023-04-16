22262dacdad9344b7a6946486d4e0e7d117cfe60
kanghyoseong <heydotory@gmail.com>
2023-04-16 21:46:53 +0900

    카메라가 배경을 넘어가지 않도록 함

--- VampireSurvivors/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/vampiresurvivors/game/Camera.java
+++ VampireSurvivors/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/vampiresurvivors/game/Camera.java
@@ -1,5 +1,6 @@
 package kr.ac.tukorea.www.smgp2018184001.vampiresurvivors.game;
 
+import android.graphics.RectF;
 import android.util.Log;
 
 import kr.ac.tukorea.www.smgp2018184001.vampiresurvivors.framework.Metrics;
@@ -7,14 +8,31 @@ import kr.ac.tukorea.www.smgp2018184001.vampiresurvivors.framework.Object;
 
 public class Camera extends Object {
     private static final String TAG = Camera.class.getSimpleName();
+    RectF boundary;
 
     public Camera(Player player) {
         super(player.getPosX(), player.getPosY());
+        setBoundary();
     }
 
     public void update(Player player) {
-        posX = player.getPosX() - Metrics.game_width * 0.5f;
-        posY = player.getPosY() - Metrics.game_height * 0.5f;
+        float newX = player.getPosX() - Metrics.game_width * 0.5f;
+        float newY = player.getPosY() - Metrics.game_height * 0.5f;
         //Log.d(TAG, "posX: " + posX + ", posY: " + posY);
+        if (newX - Metrics.game_width / 2 > boundary.left &&
+                newX + Metrics.game_width / 2 < boundary.right) {
+            posX = newX;
+        }
+        if (newY - Metrics.game_height / 2 > boundary.top &&
+                newY + Metrics.game_height / 2 < boundary.bottom) {
+            posY = newY;
+        }
+    }
+
+    void setBoundary() {
+        boundary = new RectF(-SpriteSize.BACKGROUND_SIZE / 2,
+                -SpriteSize.BACKGROUND_SIZE / 2,
+                SpriteSize.BACKGROUND_SIZE / 2,
+                SpriteSize.BACKGROUND_SIZE / 2);
     }
 }

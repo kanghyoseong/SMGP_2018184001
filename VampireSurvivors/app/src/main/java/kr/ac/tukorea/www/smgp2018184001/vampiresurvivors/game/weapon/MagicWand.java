a3989285194908eed483d8defbcd6ee03d980662
kanghyoseong <heydotory@gmail.com>
2023-05-20 11:14:53 +0900

    MagicWand가 attack을 projectileCount만큼 하되 각 attack 사이에 일정 간격을 두었다.

--- VampireSurvivors/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/vampiresurvivors/game/weapon/MagicWand.java
+++ VampireSurvivors/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/vampiresurvivors/game/weapon/MagicWand.java
@@ -15,6 +15,9 @@ import kr.ac.tukorea.www.smgp2018184001.vampiresurvivors.game.objects.Weapon;
 
 public class MagicWand extends Weapon implements IAttackable {
     private static final String TAG = MagicWand.class.getSimpleName();
+    protected float time_between_attack;
+    protected float time_between_attack_elapsedTime = 0;
+    protected int callIndex;
 
     public MagicWand(Player player) {
         super(WeaponType.MagicWand, SpriteSize.WHIP_SIZE_X, SpriteSize.WHIP_SIZE_Y,
@@ -25,11 +28,35 @@ public class MagicWand extends Weapon implements IAttackable {
         // 때문에 magicwand만의 이미지를 준다. 그 외 size값은 상관이 없다.
         atk = 10;
         elapsedCoolTime = maxCoolTime = 1.2f;
+        time_between_attack = 0.1f;
     }
 
     @Override
     public void draw(Canvas canvas) {
         // Magic Wand는 Wand Bullet을 소환하는 역할만 한다.
+    }
+
+    @Override
+    public void update(float eTime) {
+        if (elapsedCoolTime > 0) {
+            elapsedCoolTime -= eTime;
+        } else {
+            if (time_between_attack_elapsedTime <= 0) {
+                attack(callIndex);
+                callIndex++;
+                time_between_attack_elapsedTime = time_between_attack;
+                if (callIndex >= projectileCount) {
+                    time_between_attack_elapsedTime = 0;
+                    callIndex = 0;
+                    elapsedCoolTime = maxCoolTime * player.getCoolTimeRatio();
+                    isAttacking = false;
+                }
+            }
+            if (time_between_attack_elapsedTime > 0) {
+                time_between_attack_elapsedTime -= eTime;
+            }
+        }
+        //Log.d(TAG, "isAtk: " + isAttacking + String.format(", eTime: %.1f, atkETime: %.1f", elapsedCoolTime, time_between_attack_elapsedTime));
     }
 
     @Override
@@ -51,5 +78,6 @@ public class MagicWand extends Weapon implements IAttackable {
         bullet.setAtk(atk * player.getAttackRatio());
         bullet.setDir(dx, dy);
         BaseScene.getTopScene().add(MainScene.Layer.weapon, bullet);
+        //Log.d(TAG, "attack " + callIndex);
     }
 }

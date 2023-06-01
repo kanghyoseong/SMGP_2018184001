6a13bbc8365454efb039afc2af9398d669c93a96
kanghyoseong <heydotory@gmail.com>
2023-06-01 11:53:07 +0900

    메인 메뉴 생성

---
+++ VampireSurvivors/app/src/main/java/kr/ac/tukorea/www/smgp2018184001/vampiresurvivors/game/app/TitleActivity.java
@@ -0,0 +1,25 @@
+package kr.ac.tukorea.www.smgp2018184001.vampiresurvivors.game.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import kr.ac.tukorea.www.smgp2018184001.vampiresurvivors.R;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+
+    public void onBtnStart(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void onBtnSettings(View view) {
+    }
+}
